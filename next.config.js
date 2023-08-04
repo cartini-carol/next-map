e968e1bfc902208f506ee75f66caad0860a5d341
cartini-carol <carol@cartini.co.kr>
2023-08-04 12:14:44 +0900

    ing: 빌드 환경 테스트

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? " https://cartini-carol.github.io/next-map/"
+      : "",
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
